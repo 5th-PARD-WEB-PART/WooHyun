33d5f404797ce2b1a15da89f4c145c0381b31a47
woo-hyun <kwh77974481@gmail.com>
2025-03-31 12:04:40 +0900

    add: 2nd seminar example

--- 2th_seminar_example/src/pages/detail/index.tsx
+++ 2th_seminar_example/src/pages/detail/index.tsx
@@ -1,21 +1,21 @@
 import Pard from "@/components/pard";
-import Link from "next/link";
 import Head from "next/head";
+import Link from "next/link";
 
-export default function Home() {
+export default function Detail() {
   return (
     <>
       <Head>
-        <title>Pard 🌊 - 2nd Seminar Practice - Home</title>
+        <title>Pard 🌊 - 2nd Seminar Practice - detail</title>
       </Head>
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <Pard />
-        <h1 className="text-4xl font-bold text-center bg-amber-500">
-          2nd seminar practice
+        <h1 className="text-4xl font-bold text-center bg-blue-500">
+          Detail page
         </h1>
-        <Link href="/detail">
-          <div className="text-2xl font-bold text-center text-amber-500 ">
-            Go to detail page
+        <Link href="/">
+          <div className="text-2xl font-bold text-center text-blue-500 ">
+            Go to home page
           </div>
         </Link>
       </div>
