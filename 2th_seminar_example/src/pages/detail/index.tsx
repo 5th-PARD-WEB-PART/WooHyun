3df83d63643c79698d979243e6ba4926d90e6c2a
woo-hyun <kwh77974481@gmail.com>
2025-03-31 19:43:44 +0900

    fix: code compact

--- 2th_seminar_example/src/pages/detail/index.tsx
+++ 2th_seminar_example/src/pages/detail/index.tsx
@@ -1,24 +1,16 @@
-import Pard from "@/components/pard";
-import Head from "next/head";
 import Link from "next/link";
 
 export default function Detail() {
   return (
-    <>
-      <Head>
-        <title>Pard 🌊 - 2nd Seminar Practice - detail</title>
-      </Head>
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <Pard />
-        <h1 className="text-4xl font-bold text-center bg-blue-500">
-          Detail page
-        </h1>
-        <Link href="/">
-          <div className="text-2xl font-bold text-center text-blue-500 ">
-            Go to home page
-          </div>
-        </Link>
-      </div>
-    </>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-4xl font-bold text-center bg-blue-500">
+        Detail page
+      </h1>
+      <Link href="/">
+        <div className="text-2xl font-bold text-center text-blue-500 ">
+          Go to home page
+        </div>
+      </Link>
+    </div>
   );
 }
--- 2th_seminar_example/src/pages/index.tsx
+++ 2th_seminar_example/src/pages/index.tsx
@@ -1,24 +1,16 @@
-import Pard from "@/components/pard";
 import Link from "next/link";
-import Head from "next/head";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Pard 🌊 - 2nd Seminar Practice - Home</title>
-      </Head>
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <Pard />
-        <h1 className="text-4xl font-bold text-center bg-amber-500">
-          2nd seminar practice
-        </h1>
-        <Link href="/detail">
-          <div className="text-2xl font-bold text-center text-amber-500 ">
-            Go to detail page
-          </div>
-        </Link>
-      </div>
-    </>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-4xl font-bold text-center bg-amber-500">
+        2nd seminar practice
+      </h1>
+      <Link href="/detail">
+        <div className="text-2xl font-bold text-center text-amber-500 ">
+          Go to detail page
+        </div>
+      </Link>
+    </div>
   );
 }
